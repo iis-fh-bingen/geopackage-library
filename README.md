66453314b26bdd63879e639c802e68e6b5306a27
fpum <pumflorian@googlemail.com>
2015-09-10 15:02:06 +0200

    Importing current code.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # geopackage-library
+
+This is a Java geopackage library that supports JRE and Android.
+It basically consists of the code from the [Java-OpenMobility project](https://github.com/opengeospatial/Java-OpenMobility) developed by Augmented Technologies Ltd.
+We just removed all non library code, made some small adjustments and added Maven support.
---
+++ geopackage-api/src/main/java/com/augtech/geoapi/utils/FileIO.java
@@ -0,0 +1,74 @@
+package com.augtech.geoapi.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class FileIO {
+	
+	public static void streamCopy(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[1024];
+		int count = 0;
+		while((count = in.read(buffer)) != -1) {
+			out.write(buffer, 0, count);
+		}
+		in.close();
+		out.close();
+	}
+	
+	public static void unZipArchive(String zipFile, String outputFolder) {
+		byte[] buffer = new byte[1024];
+		 
+	     try{
+	 
+	    	//create output directory is not exists
+	    	File folder = new File(outputFolder);
+	    	if(!folder.exists()){
+	    		folder.mkdir();
+	    	}
+	 
+	    	//get the zip file content
+	    	ZipInputStream zis = 
+	    		new ZipInputStream(new FileInputStream(zipFile));
+	    	//get the zipped file list entry
+	    	ZipEntry ze = zis.getNextEntry();
+	 
+	    	while(ze!=null){
+	 
+	    	   String fileName = ze.getName();
+	           File newFile = new File(outputFolder + File.separator + fileName);
+	 
+	           System.out.println("file unzip : "+ newFile.getAbsoluteFile());
+	 
+	            //create all non exists folders
+	            //else you will hit FileNotFoundException for compressed folder
+	            new File(newFile.getParent()).mkdirs();
+	 
+	            FileOutputStream fos = new FileOutputStream(newFile);             
+	 
+	            int len;
+	            while ((len = zis.read(buffer)) > 0) {
+	       		fos.write(buffer, 0, len);
+	            }
+	 
+	            fos.close();   
+	            ze = zis.getNextEntry();
+	    	}
+	 
+	        zis.closeEntry();
+	    	zis.close();
+	 
+	    	System.out.println("Done");
+	 
+	    }catch(IOException ex){
+	       ex.printStackTrace(); 
+	    }
+		
+	}
+
+}
